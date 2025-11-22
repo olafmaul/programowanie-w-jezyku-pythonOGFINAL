b66bffdb77384570760b526223b0219d5e2d8346
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-11-22 15:32:46 +0100

    zad_4.py

---
+++ zad_4.py
@@ -0,0 +1,3 @@
+#Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range), a następnie wyświetli co drugi element.
+for i in range(1, 12, 2):
+    print(i)
