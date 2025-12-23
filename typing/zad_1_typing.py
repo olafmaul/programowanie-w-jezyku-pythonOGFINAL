33d85c9b3d9afe6498ddc8af379e5bc1a270a901
olafmaul <olaf.maul@edu.uekat.pl>
2025-12-23 15:52:06 +0100

    Delete typing directory

--- typing/zad_1_typing.py
+++
@@ -1,7 +0,0 @@
-# zad 1
-
-def print_name(name, surname):
-    print(f'Cześć {name} {surname}!')
-
-
-print_name('Olaf', 'Maul')
--- typing/zad_2_typing.py
+++
@@ -1,9 +0,0 @@
-# zad 2
-
-def multiply_numbers(x, y):
-    return x * y
-
-
-multiply_two_numbers = multiply_numbers(20, 154)
-
-print(multiply_two_numbers)
